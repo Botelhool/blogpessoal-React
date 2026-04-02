1192b1082c06f0661235ba6bb06115053c2d38ee
botelhool <leo.obotelho@outlook.com>
2026-04-02 16:21:08 -0300

    featadicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-//Aqui fica todos os componentes que aparecem na tela
-
-import Home from "./pages/home/Home"
-import Navbar from "./components/navbar/Navbar"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
+import Home from "./pages/home/Home"
+import Cadastro from "./pages/cadastro/Cadastro"
+import Login from "./pages/login/Login"
+
 
 function App() {
- 
+  
   return (
     <>
-    <Navbar/>  
-    <Home/>
-    <Footer/>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
-  
   )
 }
 
-
-export default App // Deixa a função pública com as config padrões 
+export default App
